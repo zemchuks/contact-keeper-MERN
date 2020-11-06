46fe82777efab90ae57f3f80b70801176fbd3a0d
Bruno Austin <brunoaugustine2@gmail.com>
2020-11-06 19:08:10 +0100

    update server

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express()
 connectDB()
 
 // Init Middleware
-app.use(express.json({ extended: false }))
+app.use(express.json({
+    extended: false
+}))
 
 // app.get('/', (req, res) => res.json({ msg: "welcome to the contact keeper API" }))
 
@@ -17,19 +19,20 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
+app.use(express.static('client/build'))
 // Serve static assests in production
-if(process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'))
+if (process.env.NODE_ENV === 'production') {
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' )))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
+//set static folder
+
 
 const PORT = process.env.PORT || 5000
 
 try {
     app.listen(PORT, () => console.log(`Live at 🏗  Port 🎚 ${PORT}...😊 `))
 } catch (error) {
-    app.listen(PORT, () =>  console.log(`🏗 Port ${PORT} is down...😠 `));
+    app.listen(PORT, () => console.log(`🏗 Port ${PORT} is down...😠 `));
 }
-
