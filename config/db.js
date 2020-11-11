baf073b10a2fa8e26823d1a2cd8d0cf9053ae385
Bruno Austin <brunoaugustine2@gmail.com>
2020-11-11 16:50:38 +0100

    fix mobile view

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const config = require('config')
-const db = config.get('DB_LOCAL')
+const db = config.get('mongoURI')
 
 const connectDB = async () => {
     try {
